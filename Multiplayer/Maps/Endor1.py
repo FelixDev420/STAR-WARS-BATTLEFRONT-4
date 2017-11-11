f4b122b7d66b1ec83c1762717e14387fb6e62fe3
GamingTrend <rafeole123@gmail.com>
2017-11-11 18:01:41 +0100

    Create Endor1.py

---
+++ Multiplayer/Maps/Endor1.py
@@ -0,0 +1,12 @@
+from random import *
+Endor = 1
+c.itemconfig(Endor, 250, 200, 0,)
+Endor = TextureEndor
+Tree = 150
+Tree = TextureTree
+c.move(Tree, random )
+if a == RebelPlayer
+   RebelPlayer = 1
+if a ImperialPlayer  
+   ImperialPlayer = 1
+c.itemconfig(banner1, 300, 700,)
