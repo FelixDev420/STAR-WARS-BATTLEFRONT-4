d5e6b09ae1ce23049083edc0e457a473d54b92a2
GamingTrend <rafeole123@gmail.com>
2017-11-11 18:54:42 +0100

    Update Endor1.py

--- Multiplayer/Maps/Endor1.py
+++ Multiplayer/Maps/Endor1.py
@@ -10,3 +10,11 @@ if a == RebelPlayer
 if a ImperialPlayer  
    ImperialPlayer = 1
 c.itemconfig(banner1, 300, 700,)
+sun = 1
+c.move(sun, 250, 200, 0,)
+if a == HanSolo
+   HanSolo = 1
+if a == Leia
+   Leia = 1
+if a == Bosk
+   Bosk = 1
