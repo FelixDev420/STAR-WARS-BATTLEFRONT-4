a470d1185e20ccb013b60110843134fd882b0bf9
GamingTrend <rafeole123@gmail.com>
2017-11-11 21:03:44 +0100

    Update StormtrooperPerm.py

--- Multiplayer/Imperial/StormtrooperPerm.py
+++ Multiplayer/Imperial/StormtrooperPerm.py
@@ -1,5 +1,45 @@
-move() = True
-jump() = True
-fly() = False
-sprint() = True
-shoot() = True
+move = True
+jump = True
+fly = False
+sprint = True
+shoot = True
+hit = False
+throw = False
+activate = false
+if a == jetpack
+   fly = True
+if a == RebelPlayer
+   hit = True
+if a == Blendgranate
+   throw = True
+if a == Kontaktgranate
+   throw = True
+if a == Ionengranate
+   throw = True
+if a == Rauchgranate
+   throw = True
+if a == Thermaldetonator
+   throw = True
+if a ==  Dioxis-Granate
+   throw = True
+if a == Armbrust
+   shoot = True
+if a == Cycler-Gewehr
+   shoot = True
+if a == Impulskanone
+   shoot = True
+if a == Scout-Pistole
+   shoot = True
+if a == Zielsuchender Schuss
+   shoot = True
+if a == Streugeschütz
+   shoot = True
+if a == Sprungpack
+   fly = True
+if a == Kühlzelle
+   activate = True
+if a == Explosiver Schuss
+   activate = True
+if a == Feuer bündeln
+   activate = True
+if a == 
